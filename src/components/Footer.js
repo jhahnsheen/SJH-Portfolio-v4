647ddc6514db199f80e136b952cd2e367b538c61
jhahnsheen <91440103+jhahnsheen@users.noreply.github.com>
2022-02-26 03:04:41 -0500

    added footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className='flex justify-between'>
+    <div className='flex justify-between pl-40 pr-36 py-16 text-xl font-[500]'>
       <div>
         <h3>Sheen J Hahn ⏤ 2022</h3>
       </div>
-      <div className='flex justify-around'>
-        <a href='https://github.com/jhahnsheen'>GitHub</a>
-        <a href='https://www.linkedin.com/in/jhahnsheen/'>LinkedIn</a>
-        <a href='https://stackoverflow.com/users/17011588/jhahnsheen'>StackOverflow</a>
+      <div className='flex justify-between'>
+        <a className='mx-4' href='https://github.com/jhahnsheen'>GitHub</a>
+        <a className='mx-4' href='https://www.linkedin.com/in/jhahnsheen/'>LinkedIn</a>
+        <a className='mx-4' href='https://stackoverflow.com/users/17011588/jhahnsheen'>StackOverflow</a>
       </div>
     </div>
   );
